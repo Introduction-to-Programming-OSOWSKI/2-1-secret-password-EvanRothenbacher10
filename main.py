365737f44f37f54aa35fb483e95e1657bfeef3ed
Evan <81447921+EvanRothenbacher10@users.noreply.github.com>
2022-09-20 12:45:51 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #WRITE YOUR CODE IN THIS FILE
+from re import X
+
+
+x = input("What is the password?: \n")
+
+def password(p):
+    if p == "Knights19":
+        return "ACCESS GRANTED"
+    else:
+        return "ACCESS DENIED"
+
+print(password(x))
